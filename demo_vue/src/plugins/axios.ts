ecf649346223fa1bc02f05c288596aa5881fbc3b
enevaca <noel.vm.2501@gmail.com>
2023-05-15 22:19:01 -0400

    demo jwt backend

--- demo_vue/src/plugins/axios.ts
+++ demo_vue/src/plugins/axios.ts
@@ -2,7 +2,10 @@ import Axios from 'axios'
 
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_BASE_URL_ENDPOINT,
-  headers: { 'Content-type': 'application/json' }
+  headers: { 
+    'Content-type': 'application/json',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjQsImlhdCI6MTY4Mzk5NzA1MCwiZXhwIjoxNjgzOTk3MTEwfQ.zwrkdwlCw40EEib1aFY7sJExjWvLcyR-aU19sLvS6Sk' 
+  }
 })
 
 export default axios
